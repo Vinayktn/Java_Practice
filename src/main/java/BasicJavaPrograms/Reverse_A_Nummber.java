aed5f3ca40cb476e0eb378098ee01b7d57246c52
VK075855 <Vinay.KumarTN@Cerner.com>
2022-09-07 00:55:54 +0530

    Create Reverse_A_Nummber.java

---
+++ src/main/java/BasicJavaPrograms/Reverse_A_Nummber.java
@@ -0,0 +1,23 @@
+package BasicJavaPrograms;
+
+import java.util.Scanner;
+
+public class Reverse_A_Nummber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number you want to reverse ");
+		int num = sc.nextInt();
+		int rev = 0, rem;
+		while(num!=0)
+		{
+			rem=num%10;
+			rev = rev*10+rem;
+			num=num/10;
+
+		}
+		System.out.println(rev);
+
+	}
+
+}
