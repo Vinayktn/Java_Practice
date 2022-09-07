5f415f306e9f6fbc57d2e032d129336c31b1ea68
VK075855 <Vinay.KumarTN@Cerner.com>
2022-09-07 17:51:27 +0530

    Prime number program

--- src/main/java/BasicJavaPrograms/CheckForPrime.java
+++ src/main/java/BasicJavaPrograms/CheckForPrime.java
@@ -7,25 +7,21 @@ public class CheckForPrime {
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the nnumber you wante to check");
-		int num = sc.nextInt();
+		System.out.println("Enter the number to check");
+		int num=sc.nextInt();
 		int temp=0;
-	
-		for(int i=2; i<=num-1; i++)
+
+		for(int i=2; i<=num; i++)
 		{
-			if(num%i==0)
+			if (num%i==0)
 			{
 				temp=temp+1;
 			}
 		}
-		if(temp==0)
-		{
-			System.out.println("prime number");
-		}
-		else
-		{
-			System.out.println("not prime");
-		}
+			if(temp==0)
+				System.out.println("Prime number");
+			else
+				System.out.println("Not prime");
 		sc.close();
 	}
 }
